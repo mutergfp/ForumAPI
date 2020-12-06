9b92001df3ddad2aa4aa296dbfbbf7ad62386092
Wawan4137 <erwan.gaychanteloup@gmail.com>
2020-12-07 00:21:43 +0100

    Premier Push, ajout de bootstrap et début de la barre nav dans le template racine

---
+++ src/Controller/ForumController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Utils\Forum;
+
+class ForumController extends AbstractController{
+
+    /**
+     * @Route("")
+     */
+
+     public function index(){
+         return $this->render('base.html.twig');
+     }
+
+}
